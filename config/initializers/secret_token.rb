1bbdf1f4fa6f1cf0eba41c72d5259f07e3dd4d8b
rlcohen <rich.cohen@i-intime.org>
2012-06-12 14:05:58 -0400

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Undergroundmed::Application.config.secret_token = 'd14574bbb6a15cdb86a9ab1cc6d16a9f174d9cee8529c05ed83a4f1791bb2a3b83d869d9f3aecfc01654779fabbddda920b4451741defeb82c4e5be31dfa8996'
