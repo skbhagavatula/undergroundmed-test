5c205880a3fb722a016f9077c902ec06f35ef343
rlcohen <rich.cohen@i-intime.org>
2012-06-28 11:14:37 -0400

    Removed some debugging information

--- app/mailers/mail_contact_detail.rb
+++ app/mailers/mail_contact_detail.rb
@@ -15,9 +15,6 @@ class MailContactDetail < ActionMailer::Base
         @to_address =  TO_PRODUCTION
         @subject =   SUBJECT_PRODUCTION
 
-        rack_env = ENV['RACK_ENV']
-        puts "---- RACK_ENV: "  + rack_env
-
         if(rack_env == "development" || rack_env ==nil)
           @from_address =   FROM_DEV
           @to_address =  TO_DEV
