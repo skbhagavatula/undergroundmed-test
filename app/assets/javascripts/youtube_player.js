7cacc92ea0eebd34db410ad8af5065bba568a3a7
Sharath Bhagavatula <skbhagavatula@gmail.com>
2012-07-23 00:27:14 -0400

    save

--- app/assets/javascripts/youtube_player.js
+++ app/assets/javascripts/youtube_player.js
@@ -43,5 +43,5 @@ function loadPlayer(id) {
   // All of the magic handled by SWFObject (http://code.google.com/p/swfobject/)
   swfobject.embedSWF("http://www.youtube.com/v/" + videoID +
       "?version=3&enablejsapi=1&playerapiid=player1",
-      "videoDiv", "640", "385", "9", null, null, params, atts);
+      "videoDiv", "768", "462", "9", null, null, params, atts);
 }
